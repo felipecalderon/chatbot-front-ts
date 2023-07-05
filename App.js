6f1e78b081eb5791a34c0f0f2e9b4263f086e51c
Felipe <felipe.calderon321@gmail.com>
2023-07-05 19:02:13 -0400

    agregando titulo app

--- App.js
+++ App.js
@@ -9,59 +9,10 @@ import SendChat from './src/components/Sendbox';
 import axios from 'axios';
 
 // Endpoint al backend
-axios.defaults.baseURL = 'https://chatbot-back-felipe.up.railway.app/api'
+// axios.defaults.baseURL = 'https://chatbot-back-felipe.up.railway.app/api'
+axios.defaults.baseURL = 'http://192.168.1.120:3001/api'
 
 const StyledView = styled(View);
-const tempMessages = [
-	{
-		id: 1,
-		role: 'assistant',
-		name: 'Ferretería Geoconstructor',
-		content:
-			'Buen día bienvenido a Geoconstructor, en que te puedo ayudar hoy?',
-	},
-	{
-		id: 2,
-		role: 'user',
-		name: 'Felipe',
-		content: 'Quiero saber el precio del osb ranurado colonial',
-	},
-	{
-		id: 3,
-		role: 'assistant',
-		name: 'Ferretería Geoconstructor',
-		content:
-			'El precio del tablero OSB ranurado colonial de 9.0mm de dimensiones 1.22x2.44mts es de $12.490. Puedes encontrar más información en nuestro sitio web [aquí](https://www.geoconstructor.cl/tienda/aglomerados/27141/tablero-osb-ranurado-colonial-9-0mm-1-22x2-44mts/).\n\nRecuerda que tenemos un stock de 500 unidades disponibles. Si tienes alguna otra pregunta, ¡no dudes en hacerla!',
-	},
-	{ id: 4, role: 'user', name: 'Felipe', content: 'Muchas gracias!' },
-	{
-		id: 5,
-		role: 'assistant',
-		name: 'Ferretería Geoconstructor',
-		content:
-			'De nada, ¡siempre es un placer ayudar! Si tienes alguna otra pregunta, no dudes en hacerla. Estoy aquí para asistirte.',
-	},
-	{
-		id: 'b93aff9b-8dd4-47f3-a21e-c21a1751f7b4',
-		name: 'Felipe',
-		content: 'Kkjj',
-	},
-	{
-		id: '93326035-07c2-4c87-94df-72ff1a7c8f84',
-		name: 'Felipe',
-		content: 'Hhhh',
-	},
-	{
-		id: 'c749b8bf-1602-4729-bc37-ed547d8a7960',
-		name: 'Felipe',
-		content: 'Hujjjj',
-	},
-	{
-		id: 'd6ae4c79-0d5d-48a7-9c52-4b794e16e75c',
-		name: 'Felipe',
-		content: 'Oiio',
-	},
-];
 
 const fetchMessagesIA = async (query) => {
 	try {
